03e3b7f8a58b33e3c058ab0c90e910dcc45cbc69
shrek <dean2k@live.co.uk>
2023-07-25 17:03:03 +0100

    Assembly info update

--- SARS/Properties/AssemblyInfo.cs
+++ SARS/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.10.1.36")]
-[assembly: AssemblyFileVersion("1.10.0.35")]
+[assembly: AssemblyFileVersion("1.10.1.36")]
